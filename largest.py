0caa0d0341e3d2dc46d6aade4dd3340e6f06d3ee
VENUJAN <VENUJAN@users.noreply.github.com>
2017-07-22 17:49:18 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,10 @@
+x = int(input("enter the first number ":))
+y = int(input("enter the second number":))
+z = int(input("enter the thaird number": ))
+if (x >= y) and (x >= z):
+largest = x
+elif (y >= x) and (y >= z):
+largest = y
+else
+largest = z
+print( "the largest number between",x, y,"and",z "is" largest )
